4dce7cfd95b1cafc3c0f3deac65d189e8acc84f3
ominosity <ominosity@msn.com>
2026-09-12 15:40:12 -0700

    Online isn't showing correct version. Updating again to see if it shows up.

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,25 +1,25 @@
 // wrapper for querySelector...returns matching element
 export function qs(selector, parent = document) {
-  return parent.querySelector(selector);
+    return parent.querySelector(selector);
 }
 // or a more concise version if you are into that sort of thing:
 // export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
+    return JSON.parse(localStorage.getItem(key));
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+    localStorage.setItem(key, JSON.stringify(data));
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
-  qs(selector).addEventListener('touchend', (event) => {
-    event.preventDefault();
-    callback();
-  });
-  qs(selector).addEventListener('click', callback);
+    qs(selector).addEventListener('touchend', (event) => {
+        event.preventDefault();
+        callback();
+    });
+    qs(selector).addEventListener('click', callback);
 }
 
 export function getParam(key) {
@@ -32,14 +32,14 @@ export function renderListWithTemplate(templateFn, parentElement, list, position
     if (clear) {
         parentElement.innerHTML = '';
     }
-    const htmlStrings = list.map(templateFn);
+    const htmlStrings = list.map(templateFn); 
     parentElement.insertAdjacentHTML(position, htmlStrings.join(''));
 }
 
 export function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error('Bad Response');
-  }
+    if (res.ok) {
+        return res.json();
+    } else {
+        throw new Error('Bad Response');
+    }
 }
