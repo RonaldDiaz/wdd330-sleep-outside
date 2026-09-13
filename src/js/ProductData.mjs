2291165591e38fb292bfb7749c47b2c0b9035f55
RonaldDiaz <ronalddiazz@gmail.com>
2026-09-12 20:19:35 -0400

    Fixed double quotes

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,15 +1,17 @@
-import { convertToJson } from "./utils.mjs";
+import { convertToJson } from './utils.mjs';
 
 export default class ProductData {
   constructor(category) {
     this.category = category;
     this.path = `/json/${this.category}.json`;
   }
+  
   async getData() {
     return fetch(this.path)
       .then(convertToJson)
       .then((data) => data);
   }
+
   async findProductById(id) {
     const products = await this.getData();
     return products.find((item) => item.Id === id);
