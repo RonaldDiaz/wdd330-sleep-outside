776cc949687b3394899eb9b7ff53ada08765ee13
ArturMRocha <Nemezix9@outlook.com>
2026-09-16 10:59:11 -0300

    render fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src/',
 
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
+    strictPort: true,
+  },
+
   build: {
     outDir: '../dist',
     rollupOptions: {
