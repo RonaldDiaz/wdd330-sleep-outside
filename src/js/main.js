64c51b7fb541dcf1c9bd131bf5272c15b03f4ac8
rarredondo9 <rebeca.arredondo.9@gmail.com>
2026-09-18 21:16:52 -0700

    WorkInProgress: Expland inventory

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,9 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
 import Alert from './Alert';
 import { updateCartCount, loadHeaderFooter } from './utils.mjs';
 
 loadHeaderFooter();
 
-const dataSource = new ProductData('tents');
-const listElement = document.querySelector('.product-list');
 const alerts = new Alert();
 alerts.init();
-
-const myList = new ProductList('tents', dataSource, listElement);
-myList.init();
 
 updateCartCount();
---
+++ src/js/product-listing.js
@@ -0,0 +1,13 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter, updateCartCount } from "./utils.mjs";
+
+loadHeaderFooter();
+
+const dataSource = new ProductData('tents');
+const listElement = document.querySelector('.product-list');
+
+const myList = new ProductList('tents', dataSource, listElement);
+myList.init();
+
+updateCartCount();
