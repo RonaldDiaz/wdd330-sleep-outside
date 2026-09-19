e00f2c6e794a72363e1dcba8554cad9e7c5a86aa
RonaldDiaz <ronalddiazz@gmail.com>
2026-09-19 16:40:08 -0400

    Remove from cart feature

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,17 +4,7 @@ import { loadHeaderFooter } from './utils.mjs';
 loadHeaderFooter();
 
 const container = document.querySelector('.product-list');
-const shoppingCart = new ShoppingCart(container);
+const cartFooterElement = document.querySelector('#cart-footer');
+const shoppingCart = new ShoppingCart(container, cartFooterElement);
 shoppingCart.init();
-
-const cartFooterElement = document.querySelector('.cart-footer');
-const totalAmountElement = document.getElementById('cart-total-sum');
-const total = shoppingCart.calculateCartTotal();
-if (total > 0) {
-    totalAmountElement.innerHTML = `$${total}`;
-    cartFooterElement.classList.remove('hide');
-} 
-else {
-    cartFooterElement.classList.add('hide');
-}
 
